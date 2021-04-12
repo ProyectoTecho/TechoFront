535ba3389d8b450c78b4bd374265d87a85edbf27
MauroBruno1990 <maurobruno_@hotmail.com>
2021-04-12 15:35:05 -0300

    ok

--- front/src/state/reducers/user.js
+++ front/src/state/reducers/user.js
@@ -10,7 +10,7 @@ export const setUser = createAction('SET_USER')
 export const postData = createAsyncThunk(
   "CREATE_USER",
   ({ name, lastname, email, phone, amount, time, dni, province, creditCard, typeCard}) => {
-    return axios.post(`https://proyectotecho.herokuapp.com/api/user`, {
+    return axios.post(`https://cors-anywhere.herokuapp.com/https://proyectotecho.herokuapp.com/api/user`, {
       name,
       lastname,
       email,
