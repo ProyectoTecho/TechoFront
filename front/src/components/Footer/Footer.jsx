d1caf6b4f2ba76878a1664e7948702a800ee7a08
Diego <diego-jofre@live.com.ar>
2021-04-14 16:57:36 -0300

    use history

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
 import "./index.css";
 import logoTecho from "../../assets/logotecho.png";
 
