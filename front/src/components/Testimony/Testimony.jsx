fc27332a38ed84e6cb895549dba8eabeb1f04129
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-04-14 14:07:08 -0300

    cambios estilos

--- front/src/components/Testimony/Testimony.jsx
+++ front/src/components/Testimony/Testimony.jsx
@@ -8,30 +8,30 @@ import testimonio3 from "../../assets/testimonio3.JPG";
 export default function Testimony() {
   return (
     <>
-    <div className="container" id="testimonios">
-
-    
-      <div className="container-fluid justify-content-md-center mb-5">
-        <h2 className="title">Testimonios</h2>
+    <div className="" id="testimonios">   
+      <div className="justify-content-md-center mb-5">
+        <h2 className="title">Nuestros 
+          <span className='lightblue-title'>Testimonios</span> 
+        </h2>
         <div className="row  justify-content-md-center mt-5">
-          <div className="cardBox col-md-3  cardBackground">
-            <img src={testimonio} class="card-img-top mt-2 " alt="..." />
+          <div className="cardBox-testimony col-md-3 col-sm-6">
+            <img src={testimonio} class="card-img-top mt-2 img-testimony-hover" alt="..." />
             <div className="card-body">
               <p className="cardText">
                 "Sigamos trabajando con más fuerza que nunca para construir una nueva normalidad donde todas las personas tengan un lugar seguro y se cumplan todos sus derechos."
               </p>
             </div>
           </div>
-          <div className="cardBox col-md-3">
-            <img src={testimonio1} class="card-img-top mt-2" alt="..." />
+          <div className="cardBox-testimony col-md-3 col-sm-6">
+            <img src={testimonio1} class="card-img-top mt-2 img-testimony-hover" alt="..." />
             <div className="card-body">
               <p className="cardText">
                 viviendas de emergencia construidas en asentamientos.
               </p>
             </div>
           </div>
-          <div className="cardBox col-md-3 cardBackground">
-            <img src={testimonio2} class="card-img-top mt-2" alt="..." />
+          <div className="cardBox-testimony col-md-3 col-sm-6">
+            <img src={testimonio2} class="card-img-top mt-2 img-testimony-hover" alt="..." />
             <div className="card-body">
               <p className="cardText">
                 ¿Qué te impulsó a ser voluntario? <br/>
@@ -41,8 +41,8 @@ export default function Testimony() {
               </p>
             </div>
           </div>
-          <div className="cardBox col-md-3">
-            <img src={testimonio3} class="card-img-top mt-2" alt="..." />
+          <div className="cardBox-testimony col-md-3 col-sm-6">
+            <img src={testimonio3} class="card-img-top mt-2 img-testimony-hover" alt="..." />
             <div className="card-body">
               <p className="cardText">
               “No alcanza con soñar una Argentina más justa, hay que levantarse y construirla”.
