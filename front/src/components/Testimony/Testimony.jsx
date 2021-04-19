d610b6dc07ae9b72e3a3cf08ea77f192dda9b1b5
Diego <diego-jofre@live.com.ar>
2021-04-19 14:26:20 -0300

    inicio del carrousel

--- front/src/components/Testimony/Testimony.jsx
+++ front/src/components/Testimony/Testimony.jsx
@@ -7,51 +7,27 @@ import testimonio3 from "../../assets/testimonio3-min.jpg";
 
 export default function Testimony() {
   return (
-    <>
-    <div className="" id="testimonios">   
-      <div className="justify-content-md-center mb-5">
-        <h2 className="title">Nuestros 
-          <span className='lightblue-title'>Testimonios</span> 
-        </h2>
-        <div className="row  justify-content-md-center mt-5">
-          <div className="cardBox-testimony col-md-3 col-sm-6">
-            <img src={testimonio} class="card-img-top mt-2 img-testimony-hover" alt="..." />
-            <div className="card-body">
-              <p className="cardText">
-                "Sigamos trabajando con más fuerza que nunca para construir una nueva normalidad donde todas las personas tengan un lugar seguro y se cumplan todos sus derechos."
-              </p>
-            </div>
-          </div>
-          <div className="cardBox-testimony col-md-3 col-sm-6">
-            <img src={testimonio1} class="card-img-top mt-2 img-testimony-hover" alt="..." />
-            <div className="card-body">
-              <p className="cardText">
-                viviendas de emergencia construidas en asentamientos.
-              </p>
-            </div>
-          </div>
-          <div className="cardBox-testimony col-md-3 col-sm-6">
-            <img src={testimonio2} class="card-img-top mt-2 img-testimony-hover" alt="..." />
-            <div className="card-body">
-              <p className="cardText">
-                ¿Qué te impulsó a ser voluntario? <br/>
-                "...los ojos vidriosos de lagrimas de los niños
-                cuando reciben una sorpresa. La felicidad de los ancianos al
-                sentirse no olvidado.. cosas que llenan el alma.»
-              </p>
-            </div>
-          </div>
-          <div className="cardBox-testimony col-md-3 col-sm-6">
-            <img src={testimonio3} class="card-img-top mt-2 img-testimony-hover" alt="..." />
-            <div className="card-body">
-              <p className="cardText">
-              “No alcanza con soñar una Argentina más justa, hay que levantarse y construirla”.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
-    </>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="w-50" src={testimonio} alt="First slide" />
+      <img class="w-50" src={testimonio1} alt="Second slide" />
+    </div>
+    
+    <div class="carousel-item">
+      <img class="w-50" src={testimonio2} alt="Third slide" />
+      <img class="w-50" src={testimonio3} alt="Third slide" />
+    </div>
+    
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
   );
 }
