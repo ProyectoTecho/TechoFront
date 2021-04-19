1a0cd4f058e65b8740a9b67c51aff4c48e860286
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-04-19 14:18:20 -0300

    componente realidad responsive

--- front/src/components/Reality/Reality.jsx
+++ front/src/components/Reality/Reality.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 const Reality = () => {
+  const { height, width } = useWindowDimensions();
+
+
   return (
     <>
     
@@ -11,44 +37,68 @@ const Reality = () => {
             <div className="row align-pelotitas">
 
               <h5 className="card-title2">72%</h5>
-              <div className="img-reality col-lg-3 col-sm-6 col-xs-12">
+              <div className="img-reality col-lg-3  col-xs-12">
                 <div className="">
                   <div className="card-text reality-text left-text-bolitas">
                     Sin acceso formal al servicio de energía eléctrica
                   </div>
                 </div>
               </div>
-
-              <div className="img-reality  col-lg-3 col-sm-3 col-xs-12">
-                <div className=" ">
-                  <div className="card-text reality-text right-text-bolitas">
-                    Sin acceso formal a la red de agua potable
+             {width > 992 ? 
+              <><div className="img-reality  col-lg-3  col-xs-12">
+                  <div className=" ">
+                    <div className="card-text reality-text right-text-bolitas pelotitas-responsive">
+                      Sin acceso formal a la red de agua potable
+                    </div>
                   </div>
                 </div>
-              </div>
-              <h5 className="card-title2 ">94%</h5>
+                <h5 className="card-title2 ">94%</h5>
+                </>
+                : 
+                <>
+                <h5 className="card-title2 ">94%</h5>
+                <div className="img-reality  col-lg-3  col-xs-12">
+                  <div className=" ">
+                    <div className="card-text reality-text right-text-bolitas pelotitas-responsive">
+                      Sin acceso formal a la red de agua potable
+                    </div>
+                  </div>
+                </div>               
+              </>
+              } 
 
             </div>
             <p className='img-text-map'>¡NECESITAMOS TU APOYO HOY!</p>
             <div >
-
-              <div className="row align-pelotitas">
-
+              <div className="row align-pelotitas">        
                 <h5 className="card-title2">98%</h5>
-                <div className="img-reality col-lg-3 col-sm-3 col-xs-12">
+                <div className="img-reality col-lg-3 col-xs-12">
                   <div className="">
                     <div className="card-text reality-text left-text-bolitas">Sin acceso formal a la red cloacal</div>
                   </div>
                 </div>
-
-                <div className="img-reality  col-lg-3 col-sm-3 col-xs-12">
+              {width > 992 ? 
+              <>
+               <div className="img-reality  col-lg-3 col-xs-12">
                   <div>
-                    <div className="card-text reality-text right-text-bolitas">Se inunda cada vez que llueve</div>
+                    <div className="card-text reality-text right-text-bolitas pelotitas-responsive">Se inunda cada vez que llueve</div>
                   </div>
                 </div>
                 <h5 className="card-title2 ">60%</h5>
+              </>
+              :
+              <>
+              <h5 className="card-title2 ">60%</h5>
+               <div className="img-reality  col-lg-3  col-xs-12">
+                  <div>
+                    <div className="card-text reality-text right-text-bolitas pelotitas-responsive">Se inunda cada vez que llueve</div>
+                  </div>
+                </div>              
+              </>
+              }
 
               </div>
+              
             </div>
 
           </div>
