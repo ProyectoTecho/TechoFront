e32b58badd0665af782eac558d25d5001bd33741
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-04-19 17:20:49 -0300

    corrigiendo merge

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -213,25 +213,25 @@ export default function Footer() {
                 <div className="modal-body ">
                   <div className='row '>
                     <div className="card col-lg-6 col-sm-6 col-xs-12" >
-                      <img className="card-photo-captadoras" src={captadora1} alt="Card image cap"/>
+                      <img className="card-photo-captadoras" src={captadora1} alt="Captadora1"/>
                       <div className="card-body">
                         <h5 className="card-title-captadoras">Ana Laura Bertoni</h5>
                       </div>
                     </div>
                     <div className="card col-lg-6 col-sm-6 col-xs-12" >
-                      <img className="card-photo-captadoras" src={captadora2} alt="Card image cap"/>
+                      <img className="card-photo-captadoras" src={captadora2} alt="Captadora2"/>
                       <div className="card-body">
                         <h5 className="card-title-captadoras">Casiana Miranda</h5>
                       </div>
                     </div>
                     <div className="card col-lg-6 col-sm-6 col-xs-12" >
-                      <img className="card-photo-captadoras" src={captadora3} alt="Card image cap"/>
+                      <img className="card-photo-captadoras" src={captadora3} alt="Captadora3"/>
                       <div className="card-body">
                         <h5 className="card-title-captadoras">Manuela Arrechea</h5>
                       </div>
                     </div>
                     <div className="card col-lg-6 col-sm-6 col-xs-12" >
-                      <img className="card-photo-captadoras" src={captadora4} alt="Card image cap"/>
+                      <img className="card-photo-captadoras" src={captadora4} alt="Captadora4"/>
                       <div className="card-body">
                         <h5 className="card-title-captadoras">Naiara Gómez</h5>
                       </div>
