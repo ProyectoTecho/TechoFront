0415e1b7e03c36ce2d7458cf4a29aeaa72c99fa9
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-06-11 11:40:59 -0300

    captadoras con firebase

--- front/src/components/Footer/ModalCaptadoras.jsx
+++ front/src/components/Footer/ModalCaptadoras.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import captadora1 from "../../assets/captadora1.png";
 import captadora2 from "../../assets/captadora2.png";
 import captadora3 from "../../assets/captadora3.jpg";
 import captadora4 from "../../assets/captadora4.png";
 import './index.css'
+import { db } from '../../firebase/firebase'
 
 const ModalCaptadoras = () => {
-    
+  const [captadoras, setCaptadoras] = useState("")
+
+  useEffect(() => {
+    /* cada vez que refresco la página o se actualiza un estado */
+    getCaptadoras()
+  }, [])
+
+  const getCaptadoras = async () => {
+    db
+      .collection('captadoras')
+      .onSnapshot((querySnapshot) => {
+        const docs = []
+        querySnapshot.forEach((doc) => {
+          docs.push({ ...doc.data(), id: doc.id })
+        });
+        setCaptadoras(docs)
+      })
+  }
+
+
+
+
     return (
         <div>
             <div className="modal fade" id="modalCaptadoras" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -19,27 +41,30 @@ const ModalCaptadoras = () => {
                 <div className="modal-body ">
                   <div className='row '>
                     <div className="card col-lg-6 col-sm-6 col-xs-12" >
-                      <img className="card-photo-captadoras" src={captadora1} alt="Captadora1"/>
+                      <img className="card-photo-captadoras" src={captadoras ? captadoras[0].firstLink : captadora1} alt="Captadora1"/>
                       <div className="card-body">
-                        <h5 className="card-title-captadoras">Ana Laura Bertoni</h5>
+                        <h5 className="card-title-captadoras">{captadoras ? captadoras[0].firstName : "Ana Laura Bertoni"}</h5>
                       </div>
                     </div>
+
                     <div className="card col-lg-6 col-sm-6 col-xs-12" >
-                      <img className="card-photo-captadoras" src={captadora2} alt="Captadora2"/>
+                      <img className="card-photo-captadoras" src={captadoras ? captadoras[0].secondLink : captadora2} alt="Captadora2"/>
                       <div className="card-body">
-                        <h5 className="card-title-captadoras">Casiana Miranda</h5>
+                        <h5 className="card-title-captadoras">{captadoras ? captadoras[0].secondName : "Casiana Miranda"}</h5>
                       </div>
                     </div>
+
                     <div className="card col-lg-6 col-sm-6 col-xs-12" >
-                      <img className="card-photo-captadoras" src={captadora3} alt="Captadora3"/>
+                      <img className="card-photo-captadoras" src={captadoras ? captadoras[0].thirdLink : captadora3} alt="Captadora3"/>
                       <div className="card-body">
-                        <h5 className="card-title-captadoras">Manuela Arrechea</h5>
+                        <h5 className="card-title-captadoras">{captadoras ? captadoras[0].thirdName : "Manuela Arrechea"}</h5>
                       </div>
                     </div>
+
                     <div className="card col-lg-6 col-sm-6 col-xs-12" >
-                      <img className="card-photo-captadoras" src={captadora4} alt="Captadora4"/>
+                      <img className="card-photo-captadoras" src={captadoras ? captadoras[0].fourthLink : captadora4} alt="Captadora4"/>
                       <div className="card-body">
-                        <h5 className="card-title-captadoras">Naiara Gómez</h5>
+                        <h5 className="card-title-captadoras">{captadoras ? captadoras[0].fourthName : "Naiara Gómez"}</h5>
                       </div>
                     </div>
                   </div>
