74f62091b1c7e2b74bc2cfde653744ebf4acdecf
Lucas Diaz Cano <74106256+lucasdiazcano@users.noreply.github.com>
2021-07-20 19:02:32 -0300

    Update user.js

--- front/src/state/reducers/user.js
+++ front/src/state/reducers/user.js
@@ -10,7 +10,7 @@ export const setUser = createAction('SET_USER')
 export const postData = createAsyncThunk(
   "CREATE_USER",
   ({ name, lastname, email, phone, amount, time, dni, province, creditCard, typeCard}) => {
-    return axios.post(`https://sheet.best/api/sheets/2e0ba062-fc36-4551-bda1-f559a400e2fa`, {
+    return axios.post(``, {
       name,
       lastname,
       email,
